359d6fe0fcc6e7a161214b9d3e9558eed07ee629
AlexQuantoxT <aleksa.kevresan@quantoxtechnology.com>
2021-09-27 11:12:56 +0200

    added PDOERRMODE

--- classes/ConnDB.php
+++ classes/ConnDB.php
@@ -1,5 +1,5 @@
 <?php
-namespace classes;
+namespace MyApp\classes;
 
 use PDO;
 use PDOException;
@@ -15,10 +15,10 @@ class ConnDB{
         try {
             $pdo = new PDO($db,$this->username,$this->password);
             $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
-        } catch (PDOException $th) {
-            print_r($th);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            return 'Connection failed: ' . $e->getMessage();
         }
         return $pdo;
-
     }
 }
