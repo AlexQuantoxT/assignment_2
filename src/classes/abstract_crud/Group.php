3cc65fb28cd91aa80865b1ee53d49166096f9ee7
AlexQuantoxT <aleksa.kevresan@quantoxtechnology.com>
2021-09-29 11:14:11 +0200

    added 2 abstract classes for groups and users, with abstract CRUD functions

--- src/classes/abstract_crud/Group.php
+++ src/classes/abstract_crud/Group.php
@@ -1,1 +1,9 @@
 <?php
+namespace MyApp\classes\abstract_crud;
+
+abstract class CRUDGroup{
+    abstract public function create($group_name);
+    abstract public function read($group_id);
+    abstract public function update($group_id,$group_name);
+    abstract public function delete($group_id);
+}
--- src/classes/abstract_crud/User.php
+++ src/classes/abstract_crud/User.php
@@ -1,1 +1,9 @@
 <?php
+namespace MyApp\classes\abstract_crud;
+
+abstract class CRUDGroup{
+    abstract public function create($user_name,$user_lastname,$role_id,$group_id);
+    abstract public function read($user_id);
+    abstract public function update($user_id,$user_name,$user_lastname,$role_id,$group_id);
+    abstract public function delete($user_id);
+}
